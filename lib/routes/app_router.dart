6ad7fc3456e2214ea378091fd21344adb765139d
Fernanda <fernandaferrari06@gmail.com>
2024-07-07 11:51:51 -0300

    Iniciando processo criação da página de login e cadastro

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:imake/routes/pages.dart';
 import 'package:imake/tasks/data/local/model/task_model.dart';
 import 'package:imake/tasks/presentation/pages/about_dev_screen.dart';
+import 'package:imake/tasks/presentation/pages/login_screen.dart';
 import 'package:imake/tasks/presentation/pages/new_task_screen.dart';
+import 'package:imake/tasks/presentation/pages/signup_screen.dart';
 import 'package:imake/tasks/presentation/pages/splash_screen.dart';
 import 'package:imake/tasks/presentation/pages/tasks_screen.dart';
 import 'package:imake/tasks/presentation/pages/update_task_screen.dart';
@@ -14,6 +16,14 @@ Route onGenerateRoute(RouteSettings routeSettings) {
     case Pages.initial:
       return MaterialPageRoute(
         builder: (context) => const SplashScreen(),
+      );
+    case Pages.login:
+      return MaterialPageRoute(
+        builder: (context) => const LoginScreen(),
+      );
+    case Pages.signup:
+      return MaterialPageRoute(
+        builder: (context) => const SignupScreen(),
       );
     case Pages.home:
       return MaterialPageRoute(
--- lib/routes/pages.dart
+++ lib/routes/pages.dart
@@ -1,5 +1,7 @@
 class Pages {
   static const initial = '/';
+  static const login = '/login';
+  static const signup = '/signup';
   static const home = '/home';
   static const createNewTask = '/createNewTask';
   static const updateTask = '/updateNewTask';
